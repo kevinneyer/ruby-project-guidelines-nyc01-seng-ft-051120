e87f090f9133900bbe8631cfe916bfcadc92768a
callas4ever <thetseringnorbu@gmail.com>
2020-05-26 15:37:14 -0400

    second commit Tsering

--- db/migrate/20200526191703_create_classrooms.rb
+++ db/migrate/20200526191703_create_classrooms.rb
@@ -1,4 +1,9 @@
 class CreateClassrooms < ActiveRecord::Migration[5.0]
   def change
+    create_table :classrooms do |t|
+      t.integer :student_id
+      t.integer :teacher_id
+      t.integer :session_id
+    end
   end
 end
--- db/migrate/20200526191739_create_sessions.rb
+++ db/migrate/20200526191739_create_sessions.rb
@@ -1,4 +1,9 @@
 class CreateSessions < ActiveRecord::Migration[5.0]
   def change
+    create_table :sessions do |t|
+      t.string :location
+      t.datetime  :start_time
+      t.datetime :end_time
+    end
   end
 end
